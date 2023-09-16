f4927dfb191791d1b19b6f8879be7040c51c0294
mili torres <militorres018@gmail.com>
2023-09-16 03:45:51 -0300

    navBar structure, themeToggle

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -3,8 +3,20 @@ import styles from './navBar.module.scss'
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar
-    <p>ola comoestan</p>
+    <div className={styles.container}>
+    <div className={styles.social}>
+        <img src='./github.png' alt='github' width={24}/>
+        <img src='./linkedin.png' alt='linkedin' width={24}/>
+        <img src='./email.png' alt='email' width={24}/>
+    </div>
+    <div className={styles.logo}>
+        <div>{"< We developing />"}</div>
+    </div>
+    <div className={styles.links}>
+        <a href='/'>Homepage</a>
+        <a href='/contact'>Contact</a>
+        <a href='/about'>About</a>
+</div>
     </div>
   )
 }
